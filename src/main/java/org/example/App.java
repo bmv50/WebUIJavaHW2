753fe7ef27763e616fcd27aa85677f047d1462ee
User <mrbaranov50@gmail.com>
2021-11-17 14:55:24 +0300

    3 домашка мой сайт 1 тест-кейс

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,11 @@
 package org.example;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -8,6 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.setProperty(
+                "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
+        );
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("incognito");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://dnev-nik.ru/");
+
+        WebElement webLogin = driver.findElement(By.name("login"));
+        webLogin.sendKeys("bmv50");
+
+        WebElement webPass = driver.findElement(By.name("parol"));
+        webPass.sendKeys("123456");
+
+        WebElement webSubmit = driver.findElement(By.xpath("/html/body/div[2]/div[1]/form/div/input[2]"));
+        webSubmit.click();
+
+        //driver.quit();
     }
 }
