c635ffafc335c4f2492f5aee8305a067749695a0
User <mrbaranov50@gmail.com>
2021-11-17 15:32:06 +0300

    3 домашка мой сайт 2 тест-кейс

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -30,8 +30,20 @@ public class App
         WebElement webPass = driver.findElement(By.name("parol"));
         webPass.sendKeys("123456");
 
-        WebElement webSubmit = driver.findElement(By.xpath("/html/body/div[2]/div[1]/form/div/input[2]"));
-        webSubmit.click();
+        WebElement webSubmitAuth = driver.findElement(By.xpath("/html/body/div[2]/div[1]/form/div/input[2]"));
+        webSubmitAuth.click();
+
+        WebElement webNewRecord = driver.findElement(By.name("title"));
+        webNewRecord.sendKeys("Новая запись");
+
+        WebElement webRadio = driver.findElement(By.xpath("//*[@id=\"form54\"]/form/div/div/label[2]/input"));
+        webRadio.click();
+
+        WebElement webRecord = driver.findElement(By.name("text"));
+        webRecord.sendKeys("Тестовая запись");
+
+        WebElement webSubmitRecord = driver.findElement(By.xpath("//*[@id=\"form54\"]/form/input[4]"));
+        webSubmitRecord.click();
 
         //driver.quit();
     }
