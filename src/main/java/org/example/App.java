b55c39c38173d184d698a55b54cc3bba55bcbc24
User <mrbaranov50@gmail.com>
2021-11-22 14:01:33 +0300

    3 домашка CRM 2 тест-кейс

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 
 public class App 
 {
-    public static void main( String[] args ) throws InterruptedException {
+    public static void main( String[] args ) {
         System.setProperty(
                 "webdriver.chrome.driver", "src/main/resources/chromedriver.exe"
         );
@@ -45,107 +45,48 @@ public class App
         WebElement webSubmitAuth = driver.findElement(By.id("_submit"));
         webSubmitAuth.click();
 
-        //Клик на кнопку "Проекты"
-        WebElement webProjectMenu1 = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/a/span"));
+        //Клик на кнопку "Контрагенты"
+        WebElement webProjectMenu1 = driver.findElement(By.xpath("//span[@class='title'][contains(.,'Контрагенты')]"));
         webProjectMenu1.click();
 
-        //Клик на кнопку "Мои проекты"
-        WebElement webProjectMenu2 = driver.findElement(By.xpath("//*[@id=\"main-menu\"]/ul/li[3]/ul/li[4]/a/span"));
+        //Клик на кнопку "Контакты"
+        WebElement webProjectMenu2 = driver.findElement(By.xpath("//span[@class='title'][contains(.,'Контактные лица')]"));
         webProjectMenu2.click();
 
-        //Клик на кнопку "Новый проект"
-        WebElement webNewProject = driver.findElement(By.xpath("//*[@id=\"container\"]/div[1]/div/div/div[2]/div/div/a"));
-        webNewProject.click();
+        //Клик на кнопку "Создать новый контакт"
+        WebElement webNewContact = driver.findElement(By.xpath("//a[contains(@title,'Создать контактное лицо')]"));
+        webNewContact.click();
 
-        //Ввод наименования проекта
-        WebElement webProjectName = driver.findElement(By.name("crm_project[name]"));
-        webProjectName.sendKeys("Тестовое наименование");
+        //Ввод фамилии
+        WebElement webContactFamily = driver.findElement(By.xpath("//input[contains(@name,'crm_contact[lastName]')]"));
+        webContactFamily.sendKeys("Баранов");
 
-        Thread.sleep(2000);
+        //Ввод имени
+        WebElement webContactName = driver.findElement(By.xpath("//input[contains(@name,'crm_contact[firstName]')]"));
+        webContactName.sendKeys("Максим");
 
-        //Клик на кнопку "организацию", далее по умолчанию "кнопка" не указываю
+        //Клик на организация
         WebElement webOrganization = driver.findElement(By.xpath("//span[@class='select2-chosen']" +
                 "[contains(.,'Укажите организацию')]"));
         webOrganization.click();
 
-        //С явным ожиданием надо еще поработать, пока так с задержкой, без нее иногда не успевает появиться список
+        //Выбор организации
+        WebElement webOrganizationSelect = driver.findElement(By.xpath("//div[@class='select2-result-label']" +
+                "[contains(.,'Alya')]"));
+        webOrganizationSelect.click();
+
         //Thread.sleep(5000);
 
-        //Выбор организации
-        WebElement webRecordOrg = driver.findElement(By.xpath("//div[@class='select2-result-label']" +
-                "[contains(.,'Alya')]"));
-        webRecordOrg.click();
-
-        //Клик на подразделение
-        WebElement webSubdivision = driver.findElement(By.xpath("//select[contains(@name,'crm_project[businessUnit]')]"));
-        webSubdivision.click();
-
-        //Выбор подразделения
-        WebElement webSubSelect = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/form/div[2]/div[3]" +
-                "/div/div[1]/div[2]/fieldset/div[2]/div[5]/div[2]/div/select/option[2]"));
-        webSubSelect.click();
-
-        //Клик на куратора проекта
-        WebElement webCurator = driver.findElement(By.xpath("//select[contains(@name,'crm_project[curator]')]"));
-        webCurator.click();
-
-        //Выбор куратора пректа
-        WebElement webCuratorSelect = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/form/div[2]/" +
-                "div[3]/div/div[1]/div[2]/fieldset/div[2]/div[6]/div[2]/div/select/option[10]"));
-        webCuratorSelect.click();
-
-        //Клик на руководителя проекта
-        WebElement webLeader = driver.findElement(By.xpath("//select[contains(@name,'crm_project[rp]')]"));
-        webLeader.click();
-
-        //Выбор руководителя проекта
-        WebElement webLeaderSelect = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/form/div[2]" +
-                "/div[3]/div/div[1]/div[2]/fieldset/div[2]/div[7]/div[2]/div/select/option[11]"));
-        webLeaderSelect.click();
-
-        //Клик на менеджер
-        WebElement webMeneger = driver.findElement(By.xpath("//select[@class='crm-manager-select']"));
-        webMeneger.click();
-
-        //Выбор менеджера
-        WebElement webMenegerSelect = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/form/div[2]" +
-                "/div[3]/div/div[1]/div[2]/fieldset/div[2]/div[9]/div[2]/div/select/option[3]"));
-        webMenegerSelect.click();
+        //Клик на должность
+        WebElement webPosition = driver.findElement(By.xpath("//input[contains(@name,'crm_contact[jobTitle]')]"));
+        webPosition.sendKeys("QA Engineer");
 
         //Сохраняем и закрываем
         WebElement webSaveClose = driver.findElement(By.xpath("//button[@type='submit']" +
                 "[contains(.,'Сохранить и закрыть')]"));
         webSaveClose.click();
 
-        Thread.sleep(3000);
-
-        //Блок удаления созданной записи
-        WebElement webProjectSearchClick = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]" +
-                "/div[2]/div[1]/div/div/span/div[1]/div[1]"));
-        webProjectSearchClick.click();
-
-        WebElement webProjectSearch = driver.findElement(By.xpath("//input[contains(@name,'value')]"));
-        webProjectSearch.sendKeys("Тестовое наименование");
-
-        WebElement webProjectSearchSubmit = driver.findElement(By.xpath("//button[@class='btn btn-primary " +
-                "filter-update'][contains(.,'Обновить')]"));
-        webProjectSearchSubmit.click();
-
-        Thread.sleep(5000);
-
-        WebElement webProjectSee = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[3]/div[2]/div[2]" +
-                "/div[2]/div[2]/div/table/tbody/tr/td[17]/div/ul/li[1]/a/i"));
-        webProjectSee.click();
-
-        Thread.sleep(5000);
-
-        WebElement webProjectDelete = driver.findElement(By.xpath("//i[@class='icon-trash hide-text'][contains(.,'Удалить')]"));
-        webProjectDelete.click();
-
-        WebElement webProjectDeleteOk = driver.findElement(By.xpath("//a[contains(@class,'btn ok btn-primary')]"));
-        webProjectDeleteOk.click();
-
-
+        //Удаление созданной записи в веб интерфейсе отсутствует.
 
         //driver.quit();
     }
